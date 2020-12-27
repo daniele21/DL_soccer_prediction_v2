03133eb756bd68b9dfad8fc4b131b89d996b0e3a
daniele21 <daniele.moltisanti@mail.polimi.it>
2020-12-27 19:42:19 +0100

    API - training, predict, simulation

--- api/v1/services/model_based/process.py
+++ api/v1/services/model_based/process.py
@@ -1,9 +1,13 @@
 from json import JSONDecodeError
 
 from api.v1.utils import load_models, load_model_and_config
+from scripts.constants.configs import HOME, AWAY
 from scripts.constants.paths import STATIC_DIR
+from scripts.constants.predict import PREDICT_CONFIG, LOW_RISK
+from scripts.data.postprocessing import postprocessing_test_data
 from scripts.models.evaluation import evaluate_results, thr_analysis
-from scripts.utils.checker import check_league, check_predict_paths
+from scripts.models.strategy import strategy_stats
+from scripts.utils.checker import check_league, check_predict_paths, check_evaluation_params, check_simulation_params
 from scripts.data.preprocessing import fill_inference_matches
 from scripts.models.model_inference import generate_test_data, model_inference, generate_output, real_case_inference
 
@@ -11,22 +15,24 @@ from flask import current_app as app
 from flask import make_response, jsonify, request
 
 from scripts.utils.loading import load_configs_from_paths
-# from . import configs, models
-models, configs = None, None
+from . import configs, models
+# models, configs = None, None
 
-@app.route('/api/v1/predict/<league_name>', methods=['POST'])
-def predict(league_name):
+
+
+@app.route('/api/v1/predict/', methods=['POST'])
+def predict():
     """
 
-    Args:
-        league_name: str
-
-    Requested Params: dict{'thr',
-                           'round',
-                           'home',
-                           'away',
-                           '1X_odds',
-                           'X2_odds'}
+    Requested Params: dict{'league_name':  str <OPTION_1>,
+                            'model_dir':   str <OPTION_2>,
+                            'model_name':  str <OPTION_2>,
+                            'thr',
+                            'round',
+                            'home_teams',
+                            'away_teams',
+                            '1X_odds',
+                            'X2_odds'}
 
     Returns:
         prediction: dict{'match',
@@ -36,35 +42,47 @@ def predict(league_name):
                          'pred_X2'}
     """
 
-    outcome, msg = check_league(league_name)
-    if not outcome:
-        response = make_response(msg, 404)
+    params = request.json
 
-    else:
-        matches = request.json
-        thr = matches['thr'] if 'thr' in list(matches.keys()) else None
+    response, model, config = load_model_and_config(params, models, configs)
 
-        model, config = models[league_name], configs[league_name]
+    if (not response['check']):
+        return make_response(response['msg'], 401)
 
-        league_params = config['league']
-        feat_eng = config['feat_eng']
+    league_params = config['league']
+    data_params = config['data']
+    feat_eng = config['feat_eng']
 
-        test_data = generate_test_data(league_params)
+    league_name = params['league_name'] if 'league_name' in params.keys() else league_params['league_name']
 
-        matches_df = fill_inference_matches(test_data, matches)
+    params = {**params, **league_params, **data_params}
 
-        predictions = {}
-        for field in ['home', 'away']:
-            pred, _ = model_inference(matches_df[field],
-                                      feat_eng,
-                                      model,
-                                      model_name=field,
-                                      train=False)
-            predictions[field] = pred
+    matches = {'home_teams': params['home_teams'],
+               'away_teams': params['away_teams'],
+               '1X_odds': params['1X_odds'],
+               'X2_odds': params['X2_odds']
+               }
 
-        outcome_dict = generate_output(matches_df, predictions, thr)
+    test_data = generate_test_data(params)
 
-        response = make_response(jsonify(outcome_dict), 200)
+    matches_df = fill_inference_matches(test_data, matches)
+
+    predictions = {}
+    for field in [HOME, AWAY]:
+        predict_config = PREDICT_CONFIG[league_name][LOW_RISK][field]
+        thr = predict_config['thr']
+        filter_bet = predict_config['filter_bet']
+
+        pred, _ = model_inference(matches_df[field],
+                                  feat_eng,
+                                  model,
+                                  model_name=field,
+                                  train=False)
+        predictions[field] = pred
+
+    outcome_dict = generate_output(matches_df, predictions, thr)
+
+    response = make_response(jsonify(outcome_dict), 200)
 
     return response
 
@@ -184,3 +202,49 @@ def process_thr_analysis():
 
     return response
 
+@app.route('/api/v1/process/strategy', methods=['POST'])
+def strategy_analysis():
+    """
+
+    Requested Params: dict {'league_name': OPTIONAL 1
+                            'model_dir': OPTIONAL 2
+                            'model_name': OPTIONAL 2,
+
+                            'thr_list': OPTIONAL,
+                            'filter_bet_list': OPTIONAL,
+                            'money_bet': OPTIONAL,
+                            'combo_list':OPTIONAL,
+                            'n_matches':OPTIONAL
+
+
+    Returns:
+
+    """
+
+    params = request.json
+
+    response, model, config = load_model_and_config(params, models, configs)
+
+    if not response['check']:
+        return make_response(response['msg'], 400)
+
+    league_params = config['league']
+    data_params = config['data']
+    feat_eng = config['feat_eng']
+
+    params = {**params, **league_params, **data_params}
+    # params['save_dir'] = STATIC_DIR
+
+    for field in [HOME, AWAY]:
+        params['field'] = field
+        testset, pred, true = real_case_inference(model, params, feat_eng)
+
+        eval_params = check_evaluation_params(params)
+        pred_df, _, _ = evaluate_results(true, pred, eval_params, plot=False)
+
+        simulation_params = check_simulation_params(params)
+        result_df, ckp_df = strategy_stats(testset, pred, true, simulation_params)
+
+    response = make_response(f'Strategy stats saved at {params["save_dir"]}', 200)
+
+    return response
--- api/v1/services/model_free/training.py
+++ api/v1/services/model_free/training.py
@@ -8,7 +8,7 @@ from scripts.models.model_inference import real_case_inference
 from scripts.models.model_init import model_directory, init_model
 from scripts.models.strategy import strategy_stats
 from scripts.networks.network_utils import init_network
-from scripts.utils.checker import check_training_args, check_training_params
+from scripts.utils.checker import check_training_args, check_training_params, check_data_params, check_simulation_params
 from scripts.utils.paths import init_env_paths
 from scripts.utils.saving import save_all_params
 
@@ -24,11 +24,16 @@ def training():
 
     Requested Params: dict{'league': LEAGUE_PARAMS,
                            'data': DATA_PARAMS,
-                           'model': MODEL_PARAMS}
+                           'model': MODEL_PARAMS
+                           'production': PRODUCTION_PARAMS}
 
         LEAGUE_PARAMS: dict{}
         DATA_PARAMS: dict{}
         MODEL_PARAMS: dict{}
+        PRODUCTION_PARAMS: dict{'active': bool
+                                'phase': eval / final,
+                                'stop_loss': float}
+
 
     Returns:
         json_response: dict{'model_dir': str,
@@ -51,6 +56,11 @@ def training():
 
     else:
         league_params, data_params, model_params = params['league'], params['data'], params['model']
+        production_params = params.get('production')
+
+        model_params = {**model_params, **production_params} if production_params is not None else model_params
+        data_params = {**data_params, **production_params} if production_params is not None else data_params
+
         epochs, patience = args['epochs'], args['patience']
 
         init_env_paths(model_params['version'])
@@ -61,11 +71,13 @@ def training():
         data_params['save_dir'] = model_dir
 
         # SAVING PARAMS
-        save_all_params(model_dir, league_params, data_params, model_params)
+        save_all_params(model_dir, league_params, data_params, model_params, production_params)
 
         try:
             # EXTRACTION DATA LEAGUE
-            league_csv, input_data = extract_data_league(league_params)
+            params = {**league_params, **data_params}
+            params = check_data_params(params)
+            league_csv, input_data = extract_data_league(params)
 
             # DATALOADER GENERATION
             dataloader, feat_eng, in_features = generate_dataset(input_data,
@@ -88,15 +100,129 @@ def training():
                              }
 
             params = {**league_params, **data_params}
+
             for field in [HOME, AWAY]:
                 params['field'] = field
 
                 testset, pred, true = real_case_inference(soccer_model, params, feat_eng)
                 thr_result, thr_dict, _ = thr_analysis(true, pred, params)
-                json_response[field] = thr_dict
+                json_response = {**json_response, **thr_dict}
 
-                result_home_df = strategy_stats(testset, pred, true, params)
-                result_away_df = strategy_stats(testset, pred, true, params)
+                simulation_params = check_simulation_params(params)
+                result_df = strategy_stats(testset, pred, true, simulation_params)
+
+        except Exception as error:
+
+            response = make_response({'msg': f'General Error: {error}'}, 400)
+            return response
+
+        except KeyboardInterrupt:
+            return make_response({'Interrupt':'Keyboard Interrupt'})
+
+        response = make_response(json_response, 200)
+
+    return response
+
+@app.route('/api/v1/training/production', methods=['POST'])
+def production_training():
+    """
+    Requested Args:
+        - epochs
+        - patience
+
+    Requested Params: dict{'league': LEAGUE_PARAMS,
+                           'data': DATA_PARAMS,
+                           'model': MODEL_PARAMS
+                           'production': PRODUCTION_PARAMS}
+
+        LEAGUE_PARAMS: dict{}
+        DATA_PARAMS: dict{}
+        MODEL_PARAMS: dict{}
+        PRODUCTION_PARAMS: dict{'active': bool
+                                'phase': eval / final,
+                                'stop_loss': float}
+
+
+    Returns:
+        json_response: dict{'model_dir': str,
+                            'model_name': str,
+                            'losses': list,
+                            'mean loss': float
+                             }
+    """
+
+    params = request.json
+    args = request.args
+
+    check_args = check_training_args(args)
+    check_params = check_training_params(params)
+
+    if (not check_args['check'] or not check_args['check']):
+        msg = f'> Args: {check_args["msg"]} \n> Params: {check_params["msg"]}'
+        logger.error(msg)
+        response = make_response(msg, 400)
+
+    else:
+        league_params, data_params, model_params = params['league'], params['data'], params['model']
+        production_params = params.get('production')
+
+        model_params = {**model_params, **production_params} if production_params is not None else model_params
+        data_params = {**data_params, **production_params} if production_params is not None else data_params
+
+        epochs, patience = args['epochs'], args['patience']
+
+        init_env_paths(model_params['version'])
+
+        model_name, model_dir = model_directory(league_params, data_params, model_params,
+                                                production=True)
+        model_params['name'] = model_name
+        model_params['save_dir'] = model_dir
+        data_params['save_dir'] = model_dir
+
+        # SAVING PARAMS
+        save_all_params(model_dir, league_params, data_params, model_params, production_params)
+
+        try:
+            # EXTRACTION DATA LEAGUE
+            params = {**league_params, **data_params}
+            params = check_data_params(params)
+            league_csv, input_data = extract_data_league(params)
+
+            # DATALOADER GENERATION
+            dataloader, feat_eng, in_features = generate_dataset(input_data,
+                                                                 data_params)
+
+            # NETWORK INITIALIZATION
+            network = init_network(in_features, model_params)
+
+            Model = init_model(data_params['dataset'])
+            soccer_model = Model(network, model_params, dataloader)
+
+            soccer_model.train(epochs, patience)
+
+            losses, mean_loss = soccer_model.get_losses()
+
+            json_response = {'model_dir': model_dir,
+                             'model_name': model_name,
+                             'losses': losses,
+                             'mean loss': mean_loss
+                             }
+
+            params = {**league_params, **data_params}
+            params['test_size'] = params['eval_size']
+
+            if(params['test_size'] > 0):
+                for field in [HOME, AWAY]:
+                    params['field'] = field
+
+                    testset, pred, true = real_case_inference(soccer_model, params, feat_eng)
+                    thr_result, thr_dict, _ = thr_analysis(true, pred, params)
+                    json_response = {**json_response, **thr_dict}
+
+                    simulation_params = check_simulation_params(params)
+                    result_df = strategy_stats(testset, pred, true, simulation_params)
+            else:
+                json_response = {'msg': f'Model at {model_dir}'}
 
         except Exception as error:
 
