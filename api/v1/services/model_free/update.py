6d805b9b5f3546786cea5bab9ef2eba1e3146ab3
daniele21 <daniele.moltisanti@mail.polimi.it>
2021-01-12 10:46:07 +0100

    update all leagues

--- api/v1/services/model_free/update.py
+++ api/v1/services/model_free/update.py
@@ -44,3 +44,52 @@ def update_league(league_name):
             response = make_response(f'Failed Update: {league_name} -> npm = {npm} \n {response["msg"]}', 400)
 
     return response
+
+@app.route('/api/v1/update', methods=['GET'])
+def update_all_leagues():
+    """
+    Request Args:
+                    - npm: int
+
+    """
+    npm = request.args['npm']
+
+    assert int(npm) > 0, 'NPM must be greater than 0'
+
+    response = {}
+
+    for league_name in LEAGUE.LEAGUE_NAMES:
+        response_league_name = check_league(league_name)
+        response_league_data = check_data_league(league_name, npm) if response_league_name['check'] else {
+            'check': False,
+            'msg': ''}
+
+        msg = f"League_name: {response_league_name['msg']} \nLeague_data: {response_league_data['msg']}"
+
+        if not response_league_name['check'] or not response_league_data['check']:
+            response[league_name] = msg
+
+
+        else:
+            logger.info('--------------------------------------')
+            logger.info(f'\nUpdating {league_name.upper()}\n')
+            league_params = {}
+            league_params['league_name'] = league_name
+            league_params['n_prev_match'] = npm
+            league_params['league_dir'] = DATA_DIR
+
+            update_response = update_data_league(league_params)
+
+            if update_response['check']:
+                succ_msg = f'Successful Update: {league_name} - npm={npm}'
+                logger.info(succ_msg)
+                response[league_name] = f'Successful Update: {league_name} -> npm = {npm}'
+
+            else:
+                fail_msg = f'Failed Update: {league_name} - npm={npm} \n {response["msg"]}'
+                logger.error(fail_msg)
+                response[league_name] = f'Failed Update: {league_name} -> npm = {npm} : {fail_msg}'
+
+
+
+    return make_response(response)
